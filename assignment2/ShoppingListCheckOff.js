8b5f62ff820f7f2afdd954ada2c92e4dfae4372b
vladddCp <39249705+vladddCp@users.noreply.github.com>
2019-02-20 14:19:52 +0200

    New item button

--- assignment2/ShoppingListCheckOff.js
+++ assignment2/ShoppingListCheckOff.js
@@ -7,29 +7,30 @@
         .controller('AlreadyBoughtController', AlreadyBoughtController)
         .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
 
-    
-        
     ToBuyController.$inject = ['$scope', 'ShoppingListCheckOffService'];
     function ToBuyController($scope, ShoppingListCheckOffService) {
-             
+
         var itemAdder = this;
 
         itemAdder.name = "";
         itemAdder.qty = "";
 
-        itemAdder.addItem = function(){
-          /* to be implemented */
+        itemAdder.addItem = function () {
+            ShoppingListCheckOffService.addItem($scope.newItem, $scope.newQty)
+            console.log($scope.newItem);
+            console.log($scope.newQty);
         }
 
-        itemAdder.getItems = function(){
+        itemAdder.getItems = function () {
             return ShoppingListCheckOffService.getNeededItems();
         }
 
-        itemAdder.buyItem = function(item){
+        itemAdder.buyItem = function (item) {
             ShoppingListCheckOffService.buyItem(item);
+            
         }
 
-        itemAdder.isListEmpty = function(){
+        itemAdder.isListEmpty = function () {
             return ShoppingListCheckOffService.getNeededItems().length == 0;
         }
     }
@@ -38,46 +39,46 @@
     function AlreadyBoughtController($scope, ShoppingListCheckOffService) {
         var showList = this;
 
-        showList.getItems = function(){
+        showList.getItems = function () {
             return ShoppingListCheckOffService.getBoughtItems();
         }
-        
-        showList.isListEmpty = function(){
+
+        showList.isListEmpty = function () {
             return ShoppingListCheckOffService.getBoughtItems().length == 0;
         }
     }
 
+    
     ShoppingListCheckOffService.$inject = [];
     function ShoppingListCheckOffService() {
         var service = this;
-        var neededItems = [{ name: "cookies", qty: 10 },
-                           { name: "cookies", qty: 10 },
-                           { name: "cookies", qty: 10 },
-                           { name: "cookies", qty: 10 },
-                           { name: "cookies", qty: 10 }];
+        var neededItems = [{name: "cookies", qty: 10},
+                           {name: "cookies", qty: 10},
+                           {name: "cookies", qty: 10},
+                           {name: "cookies", qty: 10},
+                           {name: "cookies", qty: 10}
+        ];
         var boughtItems = [];
 
         service.addItem = function (itemName, itemQty) {
-            // item = {
-            //     name : itemName,
-            //     qty : itemQty
-            // }
+            var item = {
+                name: itemName,
+                qty: itemQty
+            }
 
-            // neededItems.push(item);
+            neededItems.push(item);
         };
 
-        
-
-        service.buyItem = function(selectedItem){
+        service.buyItem = function (selectedItem) {
             neededItems = neededItems.filter(item => item !== selectedItem);
             boughtItems.push(selectedItem)
         };
 
-        service.getNeededItems = function(){
+        service.getNeededItems = function () {
             return neededItems;
         };
 
-        service.getBoughtItems = function(){
+        service.getBoughtItems = function () {
             return boughtItems;
         };
     }
